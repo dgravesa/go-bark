3a1082a9e3038436ddeee70649a2e7780571ed33
Daniel Graves <dgravesa@gmail.com>
2021-11-04 23:27:36 -0400

    adding thought and topic id fields to thought

--- bark/thought.go
+++ bark/thought.go
@@ -3,7 +3,9 @@ package bark
 import "time"
 
 type Thought struct {
+	ThoughtID   string    `json:"thoughtId"`
 	UserID      string    `json:"userId"`
+	TopicID     string    `json:"topicId"`
 	Text        string    `json:"thoughtText"`
 	CreatedTime time.Time `json:"createdTime"`
 }
