1d36729bdc8df2015649897d2139c9f349134040
Daniel Graves <dgravesa@gmail.com>
2021-11-04 23:32:32 -0400

    adding last edited time field to thought

--- bark/thought.go
+++ bark/thought.go
@@ -3,9 +3,10 @@ package bark
 import "time"
 
 type Thought struct {
-	ThoughtID   string    `json:"thoughtId"`
-	UserID      string    `json:"userId"`
-	TopicID     string    `json:"topicId"`
-	Text        string    `json:"thoughtText"`
-	CreatedTime time.Time `json:"createdTime"`
+	ThoughtID      string    `json:"thoughtId"`
+	UserID         string    `json:"userId"`
+	TopicID        string    `json:"topicId"`
+	Text           string    `json:"thoughtText"`
+	CreatedTime    time.Time `json:"createdTime"`
+	LastEditedTime time.Time `json:"lastEditedTime"`
 }
